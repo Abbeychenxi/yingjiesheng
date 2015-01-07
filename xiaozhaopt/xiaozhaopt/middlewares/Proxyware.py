63a70eb1d9f3339c5304c344dba60040c05295dc
陈希 <abbeychenx@gmail.com>
2015-01-07 12:21:17 +0800

    fix
    增加部分关键词

--- xiaozhaopt/xiaozhaopt/middlewares/Proxyware.py
+++ xiaozhaopt/xiaozhaopt/middlewares/Proxyware.py
@@ -4,4 +4,4 @@ class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
         # Set the location of the proxy
-        request.meta['proxy'] = "http://180.153.100.242:80"
+        request.meta['proxy'] = "http://222.45.196.17:8118"
